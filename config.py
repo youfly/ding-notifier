b6ad8bef830c2f3d8f16f337b6642f041767305c
youfly <youfly.yang@gmail.com>
2026-08-21 23:43:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import logging
 class Config:
     HOST = os.environ.get('HOST', '0.0.0.0')
     PORT = int(os.environ.get('PORT', 5000))
-   # 基础配置目录 (存放 bots.json 等)
-     CONFIG_DIR = os.environ.get('CONFIG_DIR', './config')
+    # 基础配置目录 (存放 bots.json 等)
+    CONFIG_DIR = os.environ.get('CONFIG_DIR', './config')
     
     # 确保路径是绝对路径
     if not os.path.isabs(CONFIG_DIR):
